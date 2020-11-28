55532264708e1883b4db28583ff1619ae8157365
Florian JUDITH <florian.judith.b@gmail.com>
2020-11-28 17:20:26 +0100

    Updated start/stop scripts

--- hack/vagrant/scripts/start-server.sh
+++ hack/vagrant/scripts/start-server.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
 [ $# -ne 1 ] && echo "Usage: $0 <SYB_SERVER>" && exit 1
+
 _server=${1}
-. /home/sybase/SYBASE.sh
+
+source /home/sybase/SYBASE.sh
+
 _run_file=${SYBROOT}/${SYBASE_ASE}/install/RUN_${_server}
+
 if [ -x ${_run_file} ]
 then
-        echo "Starting Sybase server ... ${_server}"
-        . ${_run_file}
+  echo "Starting Sybase server ... ${_server}"
+  source ${_run_file}
 else
-        echo "Cannot find run file ${_run_server}"
-        exit 1
+  echo "Cannot find run file ${_run_server}"
+  exit 1
 fi
