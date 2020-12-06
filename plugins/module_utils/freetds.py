38f943a8d24b44d43cc4f823f08fa9cc001317af
Florian JUDITH <florian.judith.b@gmail.com>
2020-12-06 09:23:02 +0100

    Leverage conneciton string

--- plugins/module_utils/freetds.py
+++ plugins/module_utils/freetds.py
@@ -22,18 +22,32 @@ except ImportError:
 from ansible.module_utils.basic import AnsibleModule, missing_required_lib
 
 
-def freetds_connect(module, login_user=None, login_password=None, login_host=None, login_port=None,
-                    connect_timeout=30, autocommit=False):
+def freetds_connect(module, login_user=None, login_password=None,
+                    login_host=None, login_port=None, db=None,
+                    connect_timeout=30, autocommit=False, encoding='utf-16le', odbc_driver='FreeTDS'):
+    
+    connection_string = {}
+
+    connection_string['server'] = login_host
+    connection_string['port'] = login_port
+    
+    # If login_user or login_password are given, they should override the
+    # config file
+    if odbc_driver is not None:
+        connection_string['driver'] = odbc_driver
+    if login_user is not None:
+        connection_string['uid'] = login_user
+    if login_password is not None:
+        connection_string['pwd'] = login_password
+    if db is not None:
+        connection_string['database'] = db
+    if connect_timeout is not None:
+        connection_string['timeout'] = connect_timeout
+    if encoding is not None:
+        connection_string['encoding'] = encoding
     
     # Connect to server
-    db_connection = freetds_driver.connect(
-        driver='FreeTDS',
-        server=login_host,
-        port=login_port,
-        uid=login_user,
-        pwd=login_password,
-        timeout=connect_timeout,
-    )
+    db_connection = freetds_driver.connect(autocommit=autocommit, **connection_string)
 
     # Monkey patch the Connection class to close the connection when garbage collected
     def _conn_patch(conn_self):
@@ -43,12 +57,14 @@ def freetds_connect(module, login_user=None, login_password=None, login_host=Non
 
     return db_connection.cursor(), db_connection
 
-
+# ref: https://github.com/mkleehammer/pyodbc/wiki/The-pyodbc-Module#connect
 def freetds_common_argument_spec():
     return dict(
         login_user=dict(type='str', default=None),
         login_password=dict(type='str', no_log=True),
         login_host=dict(type='str', default='localhost'),
         login_port=dict(type='int', default=5000),
+        odbc_driver=dict(type='str', default=['FreeTDS'])
         connect_timeout=dict(type='int', default=30),
+        encoding=dict(type='str', default='utf-16le'),
     )
