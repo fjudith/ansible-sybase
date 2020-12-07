d5d1e0b9b5d15f9d95358fc0f2cb0673c51b3487
Florian JUDITH <florian.judith.b@gmail.com>
2020-12-07 05:56:09 +0100

    Fixed driver initializtion

--- plugins/module_utils/freetds.py
+++ plugins/module_utils/freetds.py
@@ -11,24 +11,23 @@ from ansible.module_utils._text import to_native
 
 freetds_driver_fail_msg = 'The PyODBC (Python 2.7 and Python 3.X) module is required.'
 
-PYODBC_IMP_ERR = None
 try:
     import pyodbc as freetds_driver
 except ImportError:
-    PYODBC_IMP_ERR = traceback.format_exc()
-    pyodbc_found = False
-else:
-    pyodbc_found = True
+    try:
+        import pymssql as freetds_driver
+    except ImportError:
+        freetds_driver = None
 
 from ansible.module_utils.basic import AnsibleModule, missing_required_lib
 
 
 def freetds_connect(module, login_user=None, login_password=None, login_host=None, login_port=None,
-                    driver='FreeTDS', connect_timeout=30, autocommit=False):
+                    connect_timeout=30, autocommit=False):
     
     # Connect to server
     db_connection = freetds_driver.connect(
-        driver=driver,
+        driver='FreeTDS',
         server=login_host,
         port=login_port,
         uid=login_user,
@@ -39,7 +38,6 @@ def freetds_connect(module, login_user=None, login_password=None, login_host=Non
     # Monkey patch the Connection class to close the connection when garbage collected
     def _conn_patch(conn_self):
         conn_self.close()
-
     db_connection.__class__.__del__ = _conn_patch
     # Patched
 
@@ -52,6 +50,5 @@ def freetds_common_argument_spec():
         login_password=dict(type='str', no_log=True),
         server=dict(type='str', default='localhost'),
         port=dict(type='int', default=5000),
-        driver=dict(type='bool', default='FreeTDS'),
         connect_timeout=dict(type='int', default=30),
     )
