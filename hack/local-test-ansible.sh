4e4758a1e7ee850ca115286f39356709364fae96
Florian JUDITH <florian.judith.b@gmail.com>
2020-12-12 04:38:41 +0100

    Fixed Ansible-Test image

--- hack/local-test-ansible.sh
+++ hack/local-test-ansible.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ansible-test integration \
---docker "docker.io/sqlops/sybase-test-container:1.0.0-centos7" \
+--docker "quay.io/ansible/default-test-container:2.9.0" \
 --docker-network "docker_sybase" \
 --python "3.6" \
 -v --color --continue-on-error --diff --coverage
